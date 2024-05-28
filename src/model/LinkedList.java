c9aa62d86fc281ad4f5bc77bb08c8f8f495da47f
Arístides <ar.perez@correo.unimet.edu.ve>
2024-05-27 21:07:59 -0400

    feat: LinkedList is iterable

--- src/model/LinkedList.java
+++ src/model/LinkedList.java
@@ -1,17 +1,22 @@
 package model;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 /**
  *
  * @author Arístides Pérez & Jesús Duarte
  * @param <T> nodes can hold any object
  */
-public class LinkedList<T> {
+public class LinkedList<T> implements Iterable<T> {
 
     private Node<T> first;
+    private Node<T> last;
     private int size;
 
     public LinkedList() {
         this.first = null;
+        this.last = null;
         this.size = 0;
     }
 
@@ -25,21 +30,27 @@ public class LinkedList<T> {
 
         if (isEmpty()) {
             first = newNode;
+            last = newNode;
         } else {
-            first.setNext(newNode);
+            last.setNext(newNode);
+            last = newNode;
         }
         size++;
     }
 
     /**
-     * Get a Node from the list by his index, it can throw an
+     * Get a Node Data from the list by his index, it can throw an
      * IndexOutOfBoundsException
      *
      * @param index index from the node to request, starts at zero
-     * @return the Node object itself
+     * @return the Node data
      * @throws IndexOutOfBoundsException if the index is out of range
      */
-    public Node<T> get(int index) {
+    public T get(int index) {
+        return (T) get(index, false);
+    }
+
+    private Object get(int index, boolean nodeItSelf) {
 
         if (index < 0 || index >= size || isEmpty()) {
             throw new IndexOutOfBoundsException("Index Out of List Size: " + index);
@@ -47,12 +58,12 @@ public class LinkedList<T> {
         } else {
             Node<T> aux = first;
 
-            for (int i = 0; i <= index; i++) {
+            for (int i = 0; i < index; i++) {
 
                 aux = aux.getNext();
             }
 
-            return aux;
+            return nodeItSelf ? aux : aux.getData();
         }
 
     }
@@ -65,11 +76,13 @@ public class LinkedList<T> {
      * @throws IndexOutOfBoundsException if the index is out of range
      */
     public void delete(int index) {
-        Node<T> toDelete = get(index);
+        Node<T> toDelete = (Node<T>) get(index, true);
 
+        // not last or first case
         if (index != 0) {
-            Node<T> aux = get(index - 1);
+            Node<T> aux = (Node<T>) get(index - 1, true);
             aux.setNext(toDelete.getNext());
+            last = index == size - 1 ? aux : last;
 
         } else {
             first = first.getNext();
@@ -88,15 +101,13 @@ public class LinkedList<T> {
     public String toString() {
 
         if (!isEmpty()) {
-            Node<T> aux = first;
-            String toReturn = "";
+            String toReturn = "[";
 
-            for (int i = 0; i < size; i++) {
-                toReturn += aux.getData() + " ";
-                aux = aux.getNext();
+            for (T element : this) {
+                toReturn += element + ", ";
             }
 
-            return toReturn;
+            return toReturn.substring(0, toReturn.length() - 2) + "]";
         }
 
         return "";
@@ -110,12 +121,46 @@ public class LinkedList<T> {
         return size == 0;
     }
 
-    public Node<T> getFirst() {
-        return first;
+    public T getFirst() {
+        return first.getData();
+    }
+
+    public T getLast() {
+        return last.getData();
     }
 
     public int getSize() {
         return size;
     }
 
+    /**
+     *
+     * @return iterable linked list
+     */
+    @Override
+    public Iterator<T> iterator() {
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<T> {
+
+        private Node<T> current = first;
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException("No hay más elementos");
+            }
+
+            T value = current.getData();
+            current = current.getNext();
+            return value;
+        }
+
+    }
 }
