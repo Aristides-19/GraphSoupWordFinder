64c35a8769473bd83d5db8723dbe815c8e91b0fa
JesussDuarte <168352737+JesussDuarte@users.noreply.github.com>
2024-06-05 13:46:55 -0400

    Add files via upload

--- src/model/WordSearchModel.java
+++ src/model/WordSearchModel.java
@@ -6,10 +6,23 @@ package model;
  */
 public class WordSearchModel {
     
-    public Vertex dfsSearch(Graph graph, String[] str){
+    public static void dfsSearch(Graph graph, int startVertex){
+        boolean[] visited = new boolean[graph.getMaxVertices()];
+        dfs(graph, startVertex, visited);
+    }
+    
+    private static void dfs(Graph graph, int startVertex, boolean[] visited){
         
-        return null;
-
+        visited[startVertex] = true;
+        Vertex root = graph.getVertex(startVertex);
+        LinkedList<Vertex> neighbors = root.getEdges();
+        
+        for (var neighbor : neighbors){
+            int position = neighbor.getPosition();
+            if(!visited[position]){
+                dfs(graph, position, visited);
+            }
+        }
     }
     
     public static void bfsSearch(Graph graph, int startVertex) {
